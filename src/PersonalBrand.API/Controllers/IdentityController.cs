9f0acdea358b0caf6539bc4296db667038a23ec5
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-05-15 10:44:53 +0500

    CRUD User with Identity

--- src/PersonalBrand.API/Controllers/IdentityController.cs
+++ src/PersonalBrand.API/Controllers/IdentityController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PersonalBrand.Application.UseCases.IdentitieCases.Commands;
+using PersonalBrand.Application.UseCases.IdentitieCases.Queries;
 
 namespace PersonalBrand.API.Controllers
 {
@@ -20,6 +21,24 @@ namespace PersonalBrand.API.Controllers
         public async Task<IActionResult> Rergister(CreateUserCommand createUserCommand)
         {
             var result = await _mediator.Send(createUserCommand);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateUserCommand updateUserCommand)
+        {
+            var result = await _mediator.Send(updateUserCommand);
+
+            return Ok(result);
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUers()
+        {
+            var result = await _mediator.Send(new GetAllUsersQuery());
 
             return Ok(result);
         }
